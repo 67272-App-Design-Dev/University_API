3f6634be4042ebe52f4b334918d0dc0160a2c607
Larry Heimann (Prof. H) <profh@cmu.edu>
2026-03-12 13:32:03 -0400

    Change serialization method for departments index

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -3,7 +3,7 @@ module Api::V1
 
     def index
       @departments = Department.active.alphabetical.all
-      render json: DepartmentSerializer.new(@departments).serialized_json
+      render json: DepartmentSerializer.new(@departments).serializable_hash
     end
     
   end
